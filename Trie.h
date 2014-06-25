237130d3a900594ba79c3bebf1dd0aa1d4d780a9
malaga-joker <michealqiuchengwei@gmail.com>
2014-06-25 15:13:48 -0700

    Update Trie.h

--- Trie.h
+++ Trie.h
@@ -7,29 +7,29 @@ using namespace std;
 class Trie{
 private:
 	struct Node{
-		  Node* children[size];
+            Node* children[size];
 	    bool  mark;
 	    Node() : mark(false) {
-			for(int i = 0; i < size; i++) 
-				children[i] = NULL;
-		}
+		for (int i = 0; i < size; i++) 
+		    children[i] = NULL;
+	    }
 	};
 	Node *root;
 
 	Node* createNode() {
-		  Node* node = new Node();
+	    Node* node = new Node();
 	    return node;
 	}
 	
 	void destroy(Node *root) {
-		if(root != NULL) {
-        for(int i=0; i<size; i++) {
-				  if(root->children[i]) 
-				    destroy(root->children[i]);
-		    }
-		delete root;
-		root = NULL;
-	  }
+	    if (root != NULL) {
+               for (int i=0; i<size; i++) {
+		   if (root->children[i]) 
+		       destroy(root->children[i]);
+	       }
+	    delete root;
+	    root = NULL;
+	   }
 	}
 
 public:
@@ -49,12 +49,12 @@ void Trie :: insert(string str) {
 	int len = str.length();
 	Node *p = root;
 	int c;
-	for(int i=0; i<len; i++) {
-		c = str[i]-'a';
-		if(p->children[c] == NULL){
-			p->children[c] = createNode();
-		}
-		p = p->children[c];
+	for (int i=0; i<len; i++) {
+	    c = str[i]-'a';
+	    if (p->children[c] == NULL) {
+		p->children[c] = createNode();
+	    }
+	    p = p->children[c];
 	}
 	p->mark = true;
 }
@@ -63,17 +63,17 @@ bool Trie :: search(string str) {
 	int len = str.length();
 	Node *p = root;
 	int c;
-	for(int i=0; i<len; i++) {
-		c = str[i] - 'a';
-		if(p->children[c] == NULL){
-			return false;
-		}
-		p = p->children[c];
+	for (int i=0; i<len; i++) {
+	    c = str[i] - 'a';
+	    if (p->children[c] == NULL) {
+		return false;
+	    }
+	    p = p->children[c];
 	}
-	if(p->mark == true)
+	if (p->mark == true)
 	    return true;
 	else
-		return false;
+	    return false;
 }
 
 
